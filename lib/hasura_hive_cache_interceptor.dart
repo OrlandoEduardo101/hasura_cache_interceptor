ae9a0a5d4eb8cd9fd80fb49d61d5e6f291fa4222
David Araujo <david@flutterando.com.br>
2020-09-10 09:41:24 -0300

    Alterados exports

--- lib/hasura_hive_cache_interceptor.dart
+++ lib/hasura_hive_cache_interceptor.dart
@@ -1,6 +1,4 @@
 library hasura_hive_cache_interceptor;
 
 export 'src/cache_interceptor.dart';
-export 'src/services/hive_storage_service.dart';
-export 'src/services/memory_storage_service.dart';
-export 'src/services/shared_preferences_storage_service.dart';
+export 'src/services/storage_service_interface.dart';
